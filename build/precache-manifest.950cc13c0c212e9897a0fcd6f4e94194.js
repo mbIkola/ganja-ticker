a02d7978e2189251102c99018714d39124abc24a
Nickolay Kharchevin <nickolay.kharchevin@oracle.com>
2019-07-31 13:51:02 +0300

    build dir

---
+++ build/precache-manifest.950cc13c0c212e9897a0fcd6f4e94194.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cb7b7d2802abde75325147f3decd07614aa77b506f8f542484501657aad73de3
+size 992
---
+++ build/service-worker.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:42a82f4dceba5dd58a12b1d35878581a3edd4ce2b44f3ddcc1ba5f1580e9fe4c
+size 1182
